a257cf4ffda266c7b1069a943ff729f0ca8b2007
imad921 <imadmalki92140@gmail.com>
2024-12-11 14:10:38 +0100

    VF8

---
+++ mission 9/qcm.js
@@ -0,0 +1,28 @@
+function Correction()
+{window.location.href="correction.html"}
+
+function test_age(){
+    let age = prompt("Quel est votre age?")
+    if(age<18)
+    {
+        alert("Attention accès refusé, vous êtes mineur");
+        document.body.style.backgroundColor="red";
+    }
+    else
+    {
+        ;
+        document.body.style.backgroundColor="green";
+    }
+}
+
+
+function Corriger()
+{window.location.href="correction.html"}
+
+function Effacer()
+{window.location.href="correction.html"}
+
+function accueil(){
+    document.location.href="../index.html";
+}
+
